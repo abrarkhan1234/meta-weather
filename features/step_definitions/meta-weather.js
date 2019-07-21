8e79e65f22da9cf822d084bd8253e087bac050fe
Abrar Khan <abrar.khan@bbc.co.uk>
2019-07-21 01:45:59 +0100

    additonal automation

--- features/step_definitions/meta-weather.js
+++ features/step_definitions/meta-weather.js
@@ -1,31 +1,45 @@
-const { Given, When, Then } = require('cucumber');
-const { expect } = require('chai');
+const {Given, When, Then} = require('cucumber');
+const chai = require('chai');
+const assertArrays = require('chai-arrays');
+chai.use(assertArrays);
+const expect = chai.expect;
 
 Given('Today is {int}\\/{int}\\/{int}', function(year, month, date) {
-    const datePath = `${year}\/${month}\/${date}`;
-    this.setDate(datePath);
+  const datePath = `${year}\/${month}\/${date}`;
+  this.setDate(datePath);
 });
 
 When('the location is set to {string}', function(location) {
-    return this.setLocationDetails(location);
+  return this.setLocationDetails(location);
 });
 
 When('I visit the location date endpoint {string}', function(endpoint) {
-    return this.getWeatherDataForLocationAndDate();
+  return this.getWeatherDataForLocationAndDateFromApi();
 });
 
 Then('the weather forecast state should be {string}', function(weatherState) {
-    const weatherData = this.getWeatherData();
-    expect(weatherData.weather_state_name).to.eql(weatherState);
+  const weatherData = this.getWeatherDataForLocationAndDate();
+  expect(weatherData.weather_state_name).to.eql(weatherState);
 });
 
 Then('the wind direction should be {string}', function(windDirection) {
-    const weatherData = this.getWeatherData();
-    expect(weatherData.wind_direction_compass).to.eql(windDirection);
+  const weatherData = this.getWeatherDataForLocationAndDate();
+  expect(weatherData.wind_direction_compass).to.eql(windDirection);
 });
 
 Then('the maximum temperature should be {string}', function(maxTemp) {
-    const weatherData = this.getWeatherData();
-    const temperature = Math.ceil(weatherData.max_temp);
-    expect(temperature.toString()).to.eql(maxTemp);
+  const weatherData = this.getWeatherDataForLocationAndDate();
+  const temperature = Math.ceil(weatherData.max_temp);
+  expect(temperature.toString()).to.eql(maxTemp);
+});
+
+Given('I visit the location endpoint {string}', function(locationEndpoint) {
+  return this.getWeatherDataForLocationFromApi(locationEndpoint);
+});
+
+Then('I should see the London weather forecast for the next {string} days', function (noOfDays) {
+  const weatherData = this.getWeatherDataForLocation();
+  expect(weatherData.consolidated_weather).to.be.array();
+
+  expect(weatherData.consolidated_weather).to.be.ofSize(parseInt(noOfDays));
 });
